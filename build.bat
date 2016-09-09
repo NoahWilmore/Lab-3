567d60a37031e1374db5ae600398ae5654ce4b78
Chenghao Li <cli42@students.tntech.edu>
2016-09-09 13:15:27 -0500

    modified Password.cpp

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
 @echo off
 cls
 
-set DRIVE_LETTER=%1:
-set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;c:\Windows;c:\Windows\system32
 
-g++ -I.\CSC2110 -c Password.cpp
-g++ -I.\CSC2110 -c PasswordDriver.cpp
+::set DRIVE_LETTER=%1:
+::set PATH=%DRIVE_LETTER%\MinGW\bin;%DRIVE_LETTER%\MinGW\msys\1.0\bin;c:\Windows;c:\Windows\system32
 
-g++ -L.\CSC2110 -o  Password.exe PasswordDriver.o Password.o -lCSC2110
+g++ -I. -c Password.cpp
+g++ -I. -c PasswordDriver.cpp
+
+g++ -L. -o  Password.exe PasswordDriver.o Password.o -lCSC2110
 Password <fallout.txt
 
 
